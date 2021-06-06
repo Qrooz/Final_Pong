4a7a21e20c06ede0c62d86e4c487ed5d47e7e243
Cruz Ramirez <crami119@ucr.edu>
2021-06-05 22:12:54 -0700

    finished making ball physics and AI of P2

--- source/main.c
+++ source/main.c
@@ -1,5 +1,5 @@
 /*	Author: Cruz Ramirez
- *  Partner(s) Name: 
+ *      Partner(s) Name: 
  *	Lab Section: 22
  *	Assignment: Lab 11  Exercise 1
  *	Exercise Description: [optional - include for your own benefit]
@@ -15,6 +15,8 @@
 #ifdef _SIMULATE_
 #include "simAVRHeader.h"
 #include "timer.h"
+#include <stdlib.h>
+#include <time.h>
 #endif
 
 
@@ -29,13 +31,15 @@ typedef struct _task {
 //end of task scheduler structure
 
 //shared variables
-unsigned char matrix[5][8] = {	
+unsigned char matrix[5][8] = {	//view of matrix upside down
 	{0,0,0,0,0,0,0,0},
 	{1,0,0,0,0,0,0,1},
-	{1,0,0,0,0,0,0,1},
+	{1,0,0,0,0,1,0,1},
 	{1,0,0,0,0,0,0,1},
 	{0,0,0,0,0,0,0,0}};
 unsigned char inv = 0x00;
+unsigned int R = 2; //variable to keep track of row of the ball
+unsigned int C = 5; //variable to keep track of column of the ball
 //end of shared variables
 
 
@@ -107,11 +111,281 @@ int PAD1_Tick(int state) {
 	}
 return state;
 }
-//end of first state
+//end of first task
+
+//start of ball task
+enum BAL_States{BAL_movE, BAL_movW, BAL_movNE, BAL_movSE, BAL_movNW, BAL_movSW, BAL_scoreP1, BAL_scoreP2};
+
+int BAL_Tick(int state){
+	switch(state){//state transitions for ball task
+		case BAL_movE: 
+			if((matrix[R][C-1] == 1) && (R-1 == -1)){ //checks if ball comes in contact with the paddle when parallel to the top of display
+				state = BAL_movSW;
+			}
+			else if((matrix[R][C-1] == 1) && (R+1==5)){ //checks if ball comes in contact with the paddle when parallel to the bottom of display
+				state = BAL_movNW;
+			}
+			else if((matrix[R][C-1] == 1) && (matrix[R-1][C-1] == 1) &&(matrix[R+1][C-1] == 1)){ //check if ball hits mid led of paddle
+				state = BAL_movW;
+			}
+			else if((matrix[R][C-1] == 1) && (matrix[R+1][C-1] == 1) && (matrix[R+2][C-1] == 1)){ //checks if ball hits top led of paddle
+				state = BAL_movNW;
+			}
+			else if((matrix[R][C-1] == 1) && (matrix[R-1][C-1] == 1) && (matrix[R-2][C-1] == 1)){ //checks if ball hits bottom led of paddle
+				state = BAL_movSW;
+			} 
+			else if((matrix[R][C-1] == 0) && (C-1 == 0)){
+				state = BAL_scoreP2;
+			}
+		break;
+
+		case BAL_movNE:
+			if((matrix[R][C-1] == 1) && (R-1 == -1)){ //checks if ball comes in contact with top of paddle and ceiling
+                                state = BAL_movSW;
+                        }
+			else if(R-1 == -1){ //checks if ball hits ceiling
+                                state = BAL_movSE;
+                        }
+			else if((matrix[R][C-1] == 1) && (matrix[R-1][C-1] == 1) &&(matrix[R+1][C-1] == 1)){ //check if ball hits mid led of paddle
+                                state = BAL_movW;
+                        }
+                        else if((matrix[R][C-1] == 1) && (matrix[R+1][C-1] == 1) && (matrix[R+2][C-1] == 1)){ //checks if ball hits top led of paddle
+                                state = BAL_movNW;
+                        }
+                        else if((matrix[R][C-1] == 1) && (matrix[R-1][C-1] == 1) && (matrix[R-2][C-1] == 1)){ //checks if ball hits bottom led of paddle
+                                state = BAL_movSW;
+                        }
+			else if((matrix[R-1][C-1] == 0) && (C-1 == 0)){
+				state = BAL_scoreP2;
+			}
+		break;
+
+		case BAL_movSE:
+			if(matrix[R][C-1] == 1 && (R+1 == 5)){ //checks if ball comes in contact with bottom of paddle and floor
+				state = BAL_movNW;
+			}
+			else if(R+1 == 5){ //check if ball hits the floor
+				state = BAL_movNE;
+			}
+			else if((matrix[R][C-1] == 1) && (matrix[R-1][C-1] == 1) &&(matrix[R+1][C-1] == 1)){ //check if ball hits mid led of paddle
+                                state = BAL_movW;
+                        }
+                        else if((matrix[R][C-1] == 1) && (matrix[R+1][C-1] == 1) && (matrix[R+2][C-1] == 1)){ //checks if ball hits top led of paddle
+                                state = BAL_movNW;
+                        }
+                        else if((matrix[R][C-1] == 1) && (matrix[R-1][C-1] == 1) && (matrix[R-2][C-1] == 1)){ //checks if ball hits bottom led of paddle
+                                state = BAL_movSW;
+                        }
+			else if((matrix[R+1][C-1] == 0) && (C-1 == 0)){
+				state = BAL_scoreP2;
+			}
+
+		break;
+
+		case BAL_movW:
+			if((matrix[R][C+1] == 1) && (R-1 == -1)){ //checks if ball comes in contact with the paddle when parallel to the top of display
+                                state = BAL_movSE;
+                        }
+                        else if((matrix[R][C+1] == 1) && (R+1 == 5)){ //checks if ball comes in contact with the paddle when parallel to the bottom of display
+                                state = BAL_movNE;
+                        }
+                        else if((matrix[R][C+1] == 1) && (matrix[R-1][C+1] == 1) &&(matrix[R+1][C+1] == 1)){ //check if ball hits mid led of paddle
+                                state = BAL_movE;
+                        }
+                        else if((matrix[R][C+1] == 1) && (matrix[R+1][C+1] == 1) && (matrix[R+2][C+1] == 1)){ //checks if ball hits top led of paddle
+                                state = BAL_movNE;
+                        }
+                        else if((matrix[R][C+1] == 1) && (matrix[R-1][C+1] == 1) && (matrix[R-2][C+1] == 1)){ //checks if ball hits bottom led of paddle
+                                state = BAL_movSE;
+                        }
+                        else if((matrix[R][C+1] == 0) && (C+1 == 7)){
+                                state = BAL_scoreP1;
+                        }
+
+		break;
+
+		case BAL_movNW:
+			if((matrix[R][C+1] == 1) && (R-1 == -1)){ //checks if ball comes in contact with top of paddle and ceiling
+                                state = BAL_movSE;
+                        }
+                        else if(R-1 == -1){ //checks if ball hits ceiling
+                                state = BAL_movSW;
+                        }
+			else if((matrix[R][C+1] == 1) && (matrix[R-1][C+1] == 1) &&(matrix[R+1][C+1] == 1)){ //check if ball hits mid led of paddle
+                                state = BAL_movE;
+                        }
+                        else if((matrix[R][C+1] == 1) && (matrix[R+1][C+1] == 1) && (matrix[R+2][C+1] == 1)){ //checks if ball hits top led of paddle
+                                state = BAL_movNE;
+                        }
+                        else if((matrix[R][C+1] == 1) && (matrix[R-1][C+1] == 1) && (matrix[R-2][C+1] == 1)){ //checks if ball hits bottom led of paddle
+                                state = BAL_movSE;
+                        }
+			else if((matrix[R-1][C+1] == 0) && (C+1 == 7)){
+                                state = BAL_scoreP2;
+                        }
+		break;
+
+		case BAL_movSW:
+			if(matrix[R][C+1] == 1 && (R+1 == 5)){ //checks if ball comes in contact with bottom of paddle and floor
+                                state = BAL_movNE;
+                        }
+                        else if(R+1 == 5){ //check if ball hits the floor
+                                state = BAL_movNW;
+                        }
+                        else if((matrix[R][C+1] == 1) && (matrix[R-1][C+1] == 1) &&(matrix[R+1][C+1] == 1)){ //check if ball hits mid led of paddle
+                                state = BAL_movE;
+                        }
+                        else if((matrix[R][C+1] == 1) && (matrix[R+1][C+1] == 1) && (matrix[R+2][C+1] == 1)){ //checks if ball hits top led of paddle
+                                state = BAL_movNE;
+                        }
+                        else if((matrix[R][C+1] == 1) && (matrix[R-1][C+1] == 1) && (matrix[R-2][C+1] == 1)){ //checks if ball hits bottom led of paddle
+                                state = BAL_movSE;
+                        }
+                        else if((matrix[R+1][C-1] == 0) && (C-1 == 7)){
+                                state = BAL_scoreP2;
+                        }
+
+		break;
+
+		case BAL_scoreP1:
+
+		break;
+
+		case BAL_scoreP2:
+
+		break;
+
+	default: state = BAL_movE; break;
+	}
+	switch(state){//state actions for ball task
+		case BAL_movE: //ball moving east
+			matrix[R][C] = 0;
+			C -= 1;
+			matrix[R][C] = 1;
+		break;
+
+		case BAL_movNE: //ball moving north east
+			matrix[R][C] = 0;
+			R -= 1;
+			C -= 1;
+			matrix[R][C] = 1;
+		break;
+
+		case BAL_movSE: //ball moving south east
+			matrix[R][C] = 0;
+			R += 1;
+			C -= 1;
+		       	matrix[R][C] = 1;
+		break;
+
+		case BAL_movW: //ball moving west
+			matrix[R][C] = 0;
+			C += 1;
+			matrix[R][C] = 1;
+		break;
+
+		case BAL_movNW: //ball moving north west
+			matrix[R][C] = 0;
+			R -= 1;
+			C += 1;
+			matrix[R][C] = 1;
+		break;
+
+		case BAL_movSW: //ball moving south west
+			matrix[R][C] = 0;
+			R += 1;
+			C += 1;
+			matrix[R][C] = 1;
+		break;
+
+		case BAL_scoreP1:
+		case BAL_scoreP2: 
+		default:
+		break;		
+	}
+
+return state;
+}
+//end of ball task
 
 
+//start of third task to control p2 paddle or to set AI
+enum PAD2_States {PAD2_mid, PAD2_up, PAD2_down};
+int PAD2_Tick(int state) {
+        switch(state){ //state transitions for paddle 1
 
-//start of last state to update PORTD and PORTC
+                case PAD2_mid:
+                        if((rand() % 2) == 1){
+                                if(R > 2){
+					state = PAD2_down;
+				}
+				else if(R < 2){
+					state = PAD2_up;
+				}
+                        }
+                        else{
+                                state = PAD2_mid;
+                        }
+                break;
+
+                case PAD2_up:
+                        if((rand()%2) == 1){
+                                if(R > 2){
+					state = PAD2_mid;
+				}
+                        }
+                        else{
+                                state = PAD2_up;
+                        }
+                break;
+
+                case PAD2_down:
+                        if((rand()%2) == 1){
+                        	if(R < 2){
+				state = PAD2_mid;
+				}
+			}
+                        else{
+                                state = PAD2_down;
+                        }
+                break;
+                
+		default: state = PAD2_mid; break;
+        }
+	switch(state){ //state actions
+
+                case PAD2_up:
+                        matrix[0][7] = 0x01;
+                        matrix[1][7] = 0x01;
+                        matrix[2][7] = 0x01;
+                        matrix[3][7] = 0x00;
+                        matrix[4][7] = 0x00;
+                break;
+
+                case PAD2_down:
+                        matrix[0][7] = 0x00;
+                        matrix[1][7] = 0x00;
+                        matrix[2][7] = 0x01;
+                        matrix[3][7] = 0x01;
+                        matrix[4][7] = 0x01;
+                break;
+
+                case PAD2_mid:
+                        matrix[0][7] = 0x00;
+                        matrix[1][7] = 0x01;
+                        matrix[2][7] = 0x01;
+                        matrix[3][7] = 0x01;
+                        matrix[4][7] = 0x00;
+                break;
+
+                default:
+                break;
+        }
+return state;
+}
+
+
+//start of last task to update PORTD and PORTC
 enum LED_States{LED_update1, LED_update2, LED_update3, LED_update4, LED_update5};
 
 int LED_Tick(int state){
@@ -168,7 +442,7 @@ int LED_Tick(int state){
 	}
 return state;
 }
-
+//end of last state to display the board
 
 
 int main(void) {
@@ -178,8 +452,8 @@ int main(void) {
 	DDRA = 0x00; PORTA = 0xFF;
     /* Insert your solution below */
 
-	static task task1, task2;
-	task *tasks[] = {&task1, &task2};
+	static task task1, task2, task3, task4;
+	task *tasks[] = {&task1, &task2, &task3, &task4};
 	const unsigned short numTasks = sizeof(tasks)/sizeof(task*);
 
 	const char start = -1;
@@ -191,12 +465,26 @@ int main(void) {
 
 	//task2
 	task2.state = start;
-        task2.period = 1;
+        task2.period = 700;
         task2.elapsedTime = task2.period;
-        task2.TickFct = &LED_Tick;
+        task2.TickFct = &BAL_Tick;
+
+	//task3
+	task3.state = start;
+        task3.period = 700;
+        task3.elapsedTime = task3.period;
+        task3.TickFct = &PAD2_Tick;
+
+	//task4
+	task4.state = start;
+        task4.period = 1;
+        task4.elapsedTime = task4.period;
+        task4.TickFct = &LED_Tick;
 
 	TimerSet(1);
 	TimerOn();
+
+	srand(time(0));
 
 	unsigned char i = 0x00;
     while (1) {
